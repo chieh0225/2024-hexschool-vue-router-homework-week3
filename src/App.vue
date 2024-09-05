77bcd3392f73bd2aa9267f30d1e7860dd48f6bc5
chieh <secret12501250@gmail.com>
2024-09-05 12:16:43 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink> | <RouterLink to="/about">About</RouterLink> |
+          <RouterLink to="/week3">Week3</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
